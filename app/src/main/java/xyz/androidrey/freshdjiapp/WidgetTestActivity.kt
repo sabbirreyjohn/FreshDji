44a3e784989de5dc93d666b713a67412a61b98fc
Md Sabbir Ahmed Khan <sabbir.ahmed.khan.reyjohn@gmail.com>
2023-01-05 11:29:39 +0600

    added topbar panel

--- app/src/main/java/xyz/androidrey/freshdjiapp/WidgetTestActivity.kt
+++ app/src/main/java/xyz/androidrey/freshdjiapp/WidgetTestActivity.kt
@@ -2,15 +2,16 @@ package xyz.androidrey.freshdjiapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dji.v5.ux.core.panel.topbar.TopBarPanelWidget
 
 class WidgetTestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_widget)
-       /* var topBarWidget = findViewById<TopBarPanelWidget>(R.id.tPanel)
+        var topBarWidget = findViewById<TopBarPanelWidget>(R.id.tPanel)
         var systemStatusWidget = topBarWidget.systemStatusWidget
         systemStatusWidget?.let {
 
-        }*/
+        }
     }
 }
